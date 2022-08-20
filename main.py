944f3647b1abff2d59582d1dddb3dac287e23956
1489-que <paafanasyev@gmail.com>
2022-08-20 06:31:00 +0300

    almost final

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ase.io import lammpsdata
 from ase.lattice.cubic import BodyCenteredCubic
 import numpy as np
 import ase
-import matplotlib as plt
+from matplotlib import pyplot as plt
 from ase.build import attach
 import math
 from ase import neighborlist
@@ -15,11 +15,12 @@ from ase.optimize.sciopt import SciPyFminBFGS as spo
 from ase.optimize import FIRE
 from ase.calculators.lammpsrun import LAMMPS
 
-# parameters = {'pair_style': 'eam/alloy',
-#               'pair_coeff': ['* * Li_Belash2013.eam.alloy Li']}
-#
-# files = ['Li_Belash2013.eam.alloy']
-# lammps = LAMMPS(parameters=parameters, files=files)
+
+def distance(x0, x1, dimensions):
+    delta = np.abs(x0 - x1)
+    delta = np.where(delta[0] > 0.5 * dimensions, delta[0] - dimensions, delta[0])
+    return np.array(np.sqrt((delta ** 2).sum(axis=1)))
+
 
 # сам кристалл
 a = 1
@@ -28,13 +29,12 @@ atoms = BodyCenteredCubic(directions=[[a,0,0], [0,a,0], [0,0,a]],
                           latticeconstant=3.52)
 # print(atoms.positions)
 # view(atoms)
-
+dist = atoms.cell
 # удаления атома на диагонали
 del atoms[[atom.index for atom in atoms if atom.index == 173]]
 
-# print(pos[173], '\n')
 
-# print(atoms.cell, '\n')
+# создание атома на месте удаленного с небольшим сдвигом
 c = 21.12
 r = 0
 # print(len(atoms))
@@ -48,58 +48,26 @@ rel2 = (r, r, r)
 absol2 = np.dot(rel2, atoms.cell) + (8.3, 8.3, 8.3)
 atoms.append('Li')
 atoms.positions[-1] = absol2
-
-#   создание атома в углу
-# v = 19.36
-# rel3 = (r, r, r)
-# absol3 = np.dot(rel2, atoms.cell) + (0, 0, v)
-# atoms.append('Li')
-# atoms.positions[-1] = absol3
-
+ff = atoms.cell
+epsilon = np.finfo(float).eps
 # вычисление диагонали  --- с точностью эпсилон
 pos = atoms.positions
 m = []
 for i in range(len(pos)):
-    j = 0
-    if pos[i][j] == pos[i][j + 1] == pos[i][j + 2]:
+    if (pos[i][0] == pos[i][1] == pos[i][2]) or ( (pos[i][0] == pos[i][1] == pos[i][2]) == 0 and pos[i][0] <= epsilon and pos[i][1] <= epsilon and pos[i][2] <= epsilon ):
         m.append(i)
-# print(m)
-
-# print(len(atoms))
-
-
-# view(atoms)
 
 #   координаты атомов на диагонале, где m их индексы (включая интерстишил)
 n_arr = []
 for i in range(len(m)):
     n_arr.append(pos[m[i]])
-# print(n_arr)
-
-
-
-#   оптимизация
-# atoms.calc = lammps
-# FIRE(atoms)
-
-print(atoms.cell)
-# расстояние между атомами
-
-# print(len(n_arr))
-def distance(x0, x1, dimensions):
-    delta = np.abs(x0 - x1)
-    delta = np.where(delta > 0.5 * dimensions, delta - dimensions, delta)
-    return np.array(np.sqrt((delta ** 2).sum(axis=1)))
-
-# print(distance(n_arr[11], n_arr[1], dist))
+print(n_arr)
 
 #   попытка сделать вывод системы в файл и в ламмпсе оптимизировать ее
 
 lammpsdata.write_lammps_data('bulk.lammps-data', atoms)
 
 #xyz.read_xyz('E_int_1_diag2.xyz', index=None)
-
-
 
 path = "coords_int_diag2.txt"
 
@@ -111,34 +79,41 @@ arr = arr.tolist()
 arr_mod = np.array(list(np.float_(arr)))
 
 # оптимизированный кристалл
-a1 = Atoms('Li433', positions=arr_mod)
-dist = a1.cell
+a1 = Atoms('Li433', positions=arr_mod, pbc=True)
 # view(a1)
-
-#    диагональ после оптимизации \ расстояние между атомами на диагонале
 pos_optimized = a1.positions
+#    диагональ после оптимизации \ расстояние между атомами на диагонале
+# pos_optimized = a1.positions
 dim = dist
 
 
-print(m)
-for i in range(1, len(m)):
-    d = distance(pos_optimized[m[i]], pos_optimized[m[i - 1]], dim)
-    #print(distance(pos_optimized[m[i]], pos_optimized[m[i - 1]], dim), m[i], '--', m[i-1], '\n')
-    print(d[0])
+diag_d = []
 
-print(distance(pos_optimized[m[12]], pos_optimized[m[0]], dim))
+p1 = distance(pos_optimized[m[1]], pos_optimized[m[0]], dim)
+p2 = distance(pos_optimized[m[2]], pos_optimized[m[1]], dim)
+p3 = distance(pos_optimized[m[3]], pos_optimized[m[2]], dim)
+p4 = distance(pos_optimized[m[4]], pos_optimized[m[3]], dim)
+p5 = distance(pos_optimized[m[12]], pos_optimized[m[4]], dim)
+p6 = distance(pos_optimized[m[11]], pos_optimized[m[12]], dim)
+p7 = distance(pos_optimized[m[11]], pos_optimized[m[5]], dim)
+p8 = distance(pos_optimized[m[5]], pos_optimized[m[6]], dim)
+p9 = distance(pos_optimized[m[6]], pos_optimized[m[7]], dim)
+p10 = distance(pos_optimized[m[8]], pos_optimized[m[7]], dim)
+p11 = distance(pos_optimized[m[9]], pos_optimized[m[8]], dim)
+p12 = distance(pos_optimized[m[10]], pos_optimized[m[9]], dim)
+p13 = distance(pos_optimized[m[10]], pos_optimized[m[0]], dim)
 
-# запись координат диагонали после оптимизации в новый массив
-# n_arr2 = []
-# for i in range(len(m)):
-#     n_arr2.append(pos_optimized[m[i]])
-#
-#
-# a_diag = Atoms('Li', positions=n_arr2)
-#
-# view(a_diag)
+# print(p[0])
+diag_d.extend([p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0], p8[0], p9[0], p10[0], p11[0], p12[0], p13[0]])
+print(diag_d)
 
+#   ГРАФИК
 
+print(len(diag_d))
+x = np.arange(0, 13)
+print(x)
+plt.plot(x, diag_d)
+plt.show()
 
 
 ### есть ли id атомов в асе
