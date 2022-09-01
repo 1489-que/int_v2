06a55c50d71265a548131b1e361cb17f3392d3f0
1489-que <paafanasyev@gmail.com>
2022-09-01 16:16:19 +0300

    чутка оптимизировал

--- submain.py
+++ submain.py
@@ -1,10 +1,12 @@
 from ase import Atoms
 from ase.visualize import view
 from ase.build import bulk
+import os
 from ase.io import read, write
 from ase.io import lammpsdata
 from ase.lattice.cubic import BodyCenteredCubic
 import numpy as np
+from subprocess import Popen
 import ase
 from matplotlib import pyplot as plt
 from ase.build import attach
@@ -16,8 +18,6 @@ from ase.io import xyz
 from ase.optimize.sciopt import SciPyFminBFGS as spo
 from ase.optimize import FIRE
 from ase.calculators.lammpsrun import LAMMPS
-
-
 def distance(x0, x1, dimensions):
     delta = np.abs(x1 - x0)
     if delta[0] > ( 0.5 * dimensions ):
@@ -34,82 +34,59 @@ def distance(x0, x1, dimensions):
         delta[2] = delta[2]
     return np.sqrt((delta ** 2).sum())
 
-
 # сам кристалл
 a = 1
+nsize = 6
 lattice_const = 3.52
 atoms = BodyCenteredCubic(directions=[[a,0,0], [0,a,0], [0,0,a]],
-                          size=(6,6,6), symbol='Li',
+                          size=(nsize, nsize, nsize), symbol='Li',
                           latticeconstant=lattice_const)
-# print(atoms.positions)
-# view(atoms)
-dist = 21.12
-# удаления атома на диагонали
-del atoms[[atom.index for atom in atoms if atom.index == 173]]
-
-
-# создание атома на месте удаленного с небольшим сдвигом
 c = 21.12
-r = 0
-# print(len(atoms))
-rel = (r, r, r)
-absol = np.dot(rel, atoms.cell) + (8.9, 8.9, 8.9)
-atoms.append('Li')
-atoms.positions[-1] = absol
+########################
 
-# интерстишил
-rel2 = (r, r, r)
-absol2 = np.dot(rel2, atoms.cell) + (8.3, 8.3, 8.3)
-atoms.append('Li')
-atoms.positions[-1] = absol2
-ff = atoms.cell
-epsilon = lattice_const / 100
-# вычисление диагонали  --- с точностью эпсилон
 pos = atoms.positions
-m = []
-for i in range(len(pos)):
-    if (pos[i][0] == pos[i][1] == pos[i][2]):
-        m.append(i)
+atoms.append('Li')
 
-#   координаты атомов на диагонале, где m их индексы (включая интерстишил)
-n_arr = []
-for i in range(len(m)):
-    n_arr.append(pos[m[i]])
-# print(n_arr)
+# диагональ
+d = []
+for atm in atoms:
+    if np.std(atm.position) <= lattice_const / 100:
+        d.append(atm)
+pos_int = d[6].position / 3
+atoms.positions[-1] = pos_int
+d = []
+m = []
+for atm in reversed(atoms):
+    if np.std(atm.position) <= lattice_const / 100:
+        d.append(atm)
+        m.append(atm.index)
+print(d)
+print(m)
+
+########################
 
 lammpsdata.write_lammps_data('bulk.lammps-data', atoms)
-
+#os.system(r"C:\Users\default.DESKTOP-J9F7KTV\OneDrive\desktop\GB_structures\CODE\run.bat")
 atoms_read = io.read('E_int_1_diag2.lammps-dump-text')
 pos_a_read = atoms_read.positions
-
 print(pos_a_read)
-# print(len(pos_a_read))
-print('m= ', m)
+#########################
+
 arr = []
 for i in range(len(m)):
     arr.append(pos_a_read[m[i]])
-print('\n')
-print(arr)
-
 arr_mod = np.array(list(np.float_(arr)))
-print(arr_mod)
 pos_sort = sort(arr_mod, tags=arr_mod[:, 2])
-for i in range(len(m)):
-    print(pos_sort[i])
 p = []
-c = dist
+print(pos_sort)
 for i in range(1, len(m)):
     p.append(distance(pos_sort[i], pos_sort[i - 1], c))
-
+p.append(distance(pos_sort[len(m) - 1], pos_sort[0], c))
 print(p)
 
-#   ГРАФИК
+# plot
 
-print(len(p))
-x = np.arange(0, 12)
-print(x)
+
+x = np.arange(0, len(m))
 plt.plot(x, p, 'o-')
 plt.show()
-
-
-# есть ли id атомов в асе
